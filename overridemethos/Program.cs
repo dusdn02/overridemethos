1d30096a5541ef474586e7c126bbcc52ee8cb8e6
dusdn02 <lyw9114@naver.com>
2020-06-02 10:20:02 +0900

    초기 커밋.

---
+++ overridemethos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethos
+{
+    class Program
+    {
+        int Power(int input)
+        {
+            return input * input;
+        }
+        int Power(int input, int count)
+        {
+            int num = 0;
+            for (int i = 0; i < count; i++)
+                num += input;
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
