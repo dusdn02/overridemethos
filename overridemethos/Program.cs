cd362a6aa970ef1eca334104191fbc3af54c9702
dusdn02 <lyw9114@naver.com>
2020-06-02 10:22:46 +0900

    4개 메서드

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -19,6 +19,20 @@ namespace overridemethos
                 num += input;
             return num;
         }
+        int SumAll(int end)
+        {
+            int sum = 0;
+            for (int i = 0; i < end; i++)
+                sum += i;
+            return sum;
+        }
+        int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i < end; i++)
+                sum += i;
+            return sum;
+        }
 
         static void Main(string[] args)
         {
